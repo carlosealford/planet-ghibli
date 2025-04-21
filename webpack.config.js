ae787d0fd8a2bd0c55b81cb40f2ac1f238736751
webshuriken <webshuriken@gmail.com>
2025-04-21 19:55:22 +0100

    build: update webpack.config.js to use webpack-middleware
    Enables file watching

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,29 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 // TODO: PUT CSS INAN EXTERNAL FILE ASWELL AS INTERNAL
 
 module.exports = {
   entry: {
-    main: './src/index.js',
-    film: './src/film.js',
+    main: [
+      'webpack-hot-middleware/client?reload=true',
+      './src/index.js'
+    ],
+    film: [
+      'webpack-hot-middleware/client?reload=true',
+      './src/film.js'
+    ],
   },
   mode: 'development',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
     clean: true,
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: "./src/templates/index.html",
