9d70950134119f6f05ac99133cbc218c4524cb5c
Carlos E Alford <webshuriken@outlook.com>
2025-04-21 20:18:34 +0100

    Release v1.0 (#2)
    * chore: Add logo images for site
    
    * chore: move css files to src/styles folder
    
    helps to clean up the site structure
    
    * chore: moves html files inside src/templates folder
    
    improves development structure
    
    * build: use HtmlWebpackPlugin to use the new dev structure
    
    the plugin will point to the html templates
    , gives the name of the html pages,
    and will tell it which JS files to inject to each HTML file
    
    * feat: install html-webpack-plugin package
    
    * fix: import the html-webpack-plugin
    
    * chore: import css files using new file structure in index.js
    
    * chore: import css files using new file structure in film.js
    
    * fix: film page name from gallery to film
    
    * chore: update the javascript script name to have .bundle in it
    
    * build: install file-loader to handle image files
    
    * build: add new module.rule to handle images
    
    * build: add package-log file
    
    * chore: add new image assets
    
    * chore: amend baseURL of API in film js file
    
    * refactor: hero element to dynamically load the hero image
    
    * feat: add dynamic hero image to homeView
    
    changes made in index.js
    
    * style: improve hero image layout
    
    * build: load webpack using npx
    
    * feat: Home page is ready for final styling
    
    * feat: films page loads the basic film data
    
    The modals are not ready just yet but are next
    
    * feat: film modal shows the people available in the API
    
    * feat: modal for Species ready for user
    
    * feat: add class to ul and li elements in locations modal
    
    The class for the ul and li is to help the styling of the modal content
    
    * feat: add class to li and ul elements for styling
    
    * feat: Implement controller function to extract pilots name
    
    The function takes in the url with id of the pilot
    extracts the id part and searches the Model.people array
    for a match returning the pilots name to the modal view for vehicles
    
    * style: Adjust the hero image to be dynamic
    
    * style: make <body> dynamic jto fit laaaarge screens
    
    * style: home gallery now centers between 466 and 600px
    
    * style: home gallery is now fully responsive
    
    images resize nicely around different screen sizes
    
    * docs: Add site screenshot
    
    * docs: Add screenshot to README file
    
    * chores: remove about link from navigation
    
    The site does not need a about section so let go
    
    * feat: install express, nodemon, webpack-dev/hot-middleware
    
    express runs the server,
    nodemon looks out for changes and reloads the server,
    webpack-middleware enables file watching
    
    * build: update webpack.config.js to use webpack-middleware
    
    Enables file watching
    
    * feat: add js bundle reference to film.html
    
    * feat: tell Entry JS files how to handle HMR
    
    * feat: Create server file with HMR support
    
    * docs: Finish README file
    
    ---------
    
    Co-authored-by: webshuriken <webshuriken@gmail.com>

--- src/film.js
+++ src/film.js
@@ -1,9 +1,14 @@
-import './style.css';
+import './styles/style.css';
+import './styles/film.css';
+
+if (module.hot) {
+  module.hot.accept();
+}
 
 
 // Model, use an object for each topic of information
 const filmModel = {
-  baseURL: 'https://ghibliapi.herokuapp.com/',
+  baseURL: 'https://ghibliapi.vercel.app/',
   film: {},
   people: {},
   species: {},
@@ -75,9 +80,9 @@ const filmController = {
     // with just the endpoint and data you can update any property in the model
     filmModel[endpoint] = data;
   },
-  queryAPI: function(src, id = '') {
+  queryAPI: async function(src, id = '') {
     const url = filmModel.baseURL + src + '/' + id;
-    return fetch(url).then(data => data.json());
+    return await fetch(url).then(data => data.json());
   },
   createElement: function(eleName, text = false) {
     let el = document.createElement(eleName);
@@ -85,6 +90,13 @@ const filmController = {
       el.textContent = text || 'NA';
     }
     return el;
+  },
+  getVehiclePilot: function(pilotID) {
+    // the pilot is given as a url and id is the last part
+    const splitURL = pilotID.split('/');
+    const id = splitURL.pop();
+    const pilotName = filmModel.people.filter((name) => id === name.id);
+    return pilotName[0].name;
   }
 }
 
@@ -92,203 +104,327 @@ const filmController = {
 const filmInfo = {
   ini: function(film){
     // film header
-    const h2 = document.createElement('H2')
-    h2.textContent = film.title;
+    const h1 = document.createElement('H1')
+    h1.textContent = film.title;
     const h3 = document.createElement('H3');
     h3.textContent = film.original_title;
+
+    // hero movie banner
+    const movieBanner = document.createElement('IMG');
+    movieBanner.setAttribute('alt', `movie banner for ${film.original_title_romanised}`);
+    movieBanner.setAttribute('src', film.movie_banner);
 
     // film description
     const desc = document.createElement('P');
     desc.textContent = film.description;
 
     // render the film view
-    this.render(h2, h3, desc, film);
+    const view = {title: h1, subtitle: h3, description: desc, movieBanner, film}
+    this.render(view);
   },
-  render: function(h2, h3, desc, film) {
+  render: function(view) {
     const title = document.querySelector('#pGhiFilmHeroHeader');
     const description = document.querySelector('#pGhiFilmDescBody');
+    const heroBanner = document.querySelector('#pGhiFilmHeroBanner');
     // four span are filled in order with: release_date, running_time, director, producer
     const infoList = document.querySelectorAll('#pGhiFilmInfoList > li > span');
-    // can be improved...
-    title.append(h2, h3);
-    description.appendChild(desc);
+    // movie title english, japanese
+    title.append(view.title, view.subtitle);
+    description.appendChild(view.description);
+    // hero movie banner
+    heroBanner.append(view.movieBanner)
     // film info
-    infoList[0].textContent = film.release_date;
-    infoList[1].textContent = film.running_time;
-    infoList[2].textContent = film.director;
-    infoList[3].textContent = film.producer;
+    infoList[0].textContent = view.film.release_date;
+    infoList[1].textContent = view.film.running_time;
+    infoList[2].textContent = view.film.director;
+    infoList[3].textContent = view.film.producer;
   }
 }
 
+/**
+ * Dynamic modal creation ready to be populated
+ * @params {string} title - header text for the modal, defaults to something
+ * @returns {object} with two properties, modalBody to be populated and the modalPopup who is the parent
+ */
+const filmModal = function(title = 'something') {
+  const cElem = el => document.createElement(el);
+  const cAttr = (el, at, value) => {
+    el.setAttribute(at, value);
+  }
+
+  // popup wrapper
+  const modalPopup = cElem('DIV');
+  cAttr(modalPopup, 'class', 'pghi-film__popup');
+
+  // popup header
+  const modalHeader = cElem('DIV');
+  cAttr(modalHeader, 'class', 'pghi-film__popup-header');
+  const h2 = cElem('H2');
+  h2.textContent = title;
+  const closeBtn = cElem('A');
+  closeBtn.href = '#';
+  closeBtn.textContent = 'close';
+  modalHeader.append(h2, closeBtn);
+
+  modalPopup.append(modalHeader);
+  // popup content
+  const modalBody = cElem('DIV');
+  cAttr(modalBody, 'class', 'pghi-film__popup-body');
+
+  return {modalPopup, modalBody};
+}
+
+// Creates the People modal and populates it
 const filmPeople = {
   ini: function(people, cElem) {
-    const parent = document.createElement('DIV');
+    const {modalPopup, modalBody} = filmModal('People');
 
     // list of item attributes
     people.forEach(person => {
       const article = cElem('ARTICLE');
+      article.setAttribute('class', 'pghi-card');
       const header = cElem('HEADER');
+      header.setAttribute('class', 'pghi-card__header');
       const profileBox = cElem('DIV');
-      const profile = cElem('IMG');
       const h3 = cElem('H3');
+      h3.setAttribute('class', 'pghi-card__header--title');
       const div = cElem('DIV');
+      div.setAttribute('class', 'pghi-card__body');
       const ul = cElem('UL');
+      ul.setAttribute('class', 'pghi-card__body--list');
 
       h3.textContent = person.name;
 
       // TODO: IMPROVE LIST CREATION, only doing it this way because of time constraints
       let li = cElem('LI', person.age);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       let span = cElem('SPAN', "Age: ");
       li.prepend(span);
       ul.appendChild(li);
       li = cElem('LI', person.gender);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Gender: ");
       li.prepend(span);
       ul.appendChild(li);
       li = cElem('LI', person.eye_color);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Eye colour: ");
       li.prepend(span);
       ul.appendChild(li);
       li = cElem('LI', person.hair_color);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Hair colour: ");
       li.prepend(span);
       ul.appendChild(li);
       li = cElem('LI');
       span = cElem('SPAN');
       // character header
-      profileBox.appendChild(profile);
-      header.append(profileBox, h3);
+      header.append(h3);
       div.appendChild(ul);
       // assemble the card for film individual
       article.append(header, div);
-      parent.appendChild(article);
+      modalBody.appendChild(article);
     });
 
+    modalPopup.append(modalBody);
     // now render the peoples block
-    this.render(parent);
+    this.render(modalPopup);
   },
   render: function(view) {
-    const container = document.querySelector('#pghiExtrasPeople');
+    const container = document.querySelector('#pGhiExtrasPeoplePopup');
     container.appendChild(view);
   }
 };
 
+// Creates the Species modal and populates it
 const filmSpecies = {
   ini: function(species, cElem) {
-    const parent = cElem('DIV');
+    const {modalPopup, modalBody} = filmModal('Species');
 
     species.forEach(specie => {
       const article = cElem('ARTICLE');
+      article.setAttribute('class', 'pghi-card');
       let ul = cElem('UL');
+      ul.setAttribute('class', 'pghi-card__body--list');
+      let ulL2, li, span, p;
       const header = cElem('HEADER');
+      header.setAttribute('class', 'pghi-card__header');
       const h3 = cElem('H3', specie.name);
+      h3.setAttribute('class', 'pghi-card__header--title');
       header.appendChild(h3);
+      const div = cElem('DIV');
+      div.setAttribute('class', 'pghi-card__body');
       article.append(header);
 
-      let p = cElem('P', specie.classification);
-      let span = cElem('SPAN', "Classification: ");
-      p.prepend(span);
-      article.append(p);
+      // list item - classification
+      li = cElem('LI');
+      li.setAttribute('class', 'pghi-card__body--list-item');
+      p = cElem('P');
+      p.setAttribute('class', 'pghi-card__body--list-item');
+      let textNode = document.createTextNode(specie.classification);
+      span = cElem('SPAN', "Classification: ");
+      p.prepend(span, textNode);
+      li.append(p);
+      ul.append(li);
 
-      // list of eye colours
-      p = cElem('P', "Eye Colours: ");
+      // list item - eye colours
+      li = cElem('LI');
+      li.setAttribute('class', 'pghi-card__body--list-item');
+      span = cElem('SPAN', "Eye Colours: ");
+      ulL2 = cElem('UL');
+      ulL2.setAttribute('class', 'pghi-card__body--list');
       specie.eye_colors.split(', ').forEach(color => {
         let li = cElem('LI', color);
-        ul.appendChild(li);
+        ulL2.appendChild(li);
       });
-      article.append(p, ul);
+      li.append(span, ulL2);
+      ul.append(li);
 
       // list of hair colours
-      p = cElem('P', "Hair Colours: ");
-      ul = cElem('UL');
+      li = cElem('LI');
+      li.setAttribute('class', 'pghi-card__body--list-item');
+      span = cElem('SPAN', "Hair Colours: ");
+      ulL2 = cElem('UL');
+      ulL2.setAttribute('class', 'pghi-card__body--list');
       specie.hair_colors.split(', ').forEach(color => {
         let li = cElem('LI', color);
-        ul.appendChild(li);
+        ulL2.appendChild(li);
       });
-      article.append(p, ul);
-      parent.appendChild(article);
+      li.append(span, ulL2);
+      ul.append(li);
+
+      div.append(ul);
+      article.append(div);
+      modalBody.appendChild(article);
     });
 
+    modalPopup.append(modalBody);
     // render the species block
-    this.render(parent);
+    this.render(modalPopup);
   },
   render: function(view) {
-    const container = document.querySelector('#pghiExtrasSpecies');
+    const container = document.querySelector('#pGhiExtrasSpeciesPopup');
     container.appendChild(view);
   }
 }
+
+// Creates the Locations modal and populates it
 const filmLocations = {
   ini: function(locations, cElem) {
-    const parent = cElem('DIV');
+    const {modalPopup, modalBody} = filmModal('Locations');
 
     locations.forEach(location => {
       const article = cElem('ARTICLE');
+      article.setAttribute('class', 'pghi-card');
       const header = cElem('HEADER');
+      header.setAttribute('class', 'pghi-card__header');
+      const div = cElem('DIV');
+      div.setAttribute('class', 'pghi-card__body');
       const ul = cElem('UL');
+      ul.setAttribute('class', 'pghi-card__body--list');
 
+      // list header
       const h3 = cElem('H3', location.name);
+      h3.setAttribute('class', 'pghi-card__header--title');
       header.appendChild(h3);
+
+      // climate
       let li = cElem('LI', location.climate);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       let span = cElem('SPAN', "Climate: ");
       li.prepend(span);
       ul.appendChild(li);
+
+      // terrain
       li = cElem('LI', location.terrain);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Terrain: ");
       li.prepend(span);
       ul.appendChild(li);
+
+      // surface water
       li = cElem('LI', location.surface_water);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Surface Water: ");
       li.prepend(span);
       ul.appendChild(li);
-      article.append(header, ul);
-      parent.appendChild(article);
+      div.append(ul);
+      article.append(header, div);
+      modalBody.appendChild(article);
     });
 
+    modalPopup.append(modalBody);
     // render the species block
-    this.render(parent);
+    this.render(modalPopup);
   },
   render: function(view) {
-    const container = document.querySelector('#pghiExtrasLocations');
+    const container = document.querySelector('#pGhiExtrasLocationsPopup');
     container.appendChild(view);
   }
 }
 
+// Creates the Vehicles modal and populates it
 const filmVehicles = {
   ini: function(vehicles, cElem) {
-    const parent = cElem('DIV');
+    const {modalPopup, modalBody} = filmModal('Vehicles');
 
     vehicles.forEach(vehicle => {
       const article = cElem('ARTICLE');
+      article.setAttribute('class', 'pghi-card');
       const header = cElem('HEADER');
+      header.setAttribute('class', 'pghi-card__header');
+      const div = cElem('DIV');
+      div.setAttribute('class', 'pghi-card__body');
       const profileBox = cElem('DIV');
       const profile = cElem('IMG');
       const ul = cElem('UL');
+      ul.setAttribute('class', 'pghi-card__body--list');
 
+      // list header - vehicle name
       const h3 = cElem('H3', vehicle.name);
+      h3.setAttribute('class', 'pghi-card__header--title');
       profileBox.appendChild(profile);
       header.append(profileBox, h3);
-      article.appendChild(header);
+      div.appendChild(header);
 
-      let p = cElem('P', vehicle.vehicle_class);
+      // vehicle class
+      let li = cElem('LI', vehicle.vehicle_class);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       let span = cElem('SPAN', "Vehicle Class: ");
-      p.prepend(span);
-      article.appendChild(p);
+      li.prepend(span);
+      ul.appendChild(li);
 
-      p = cElem('P', vehicle.length);
+      // vehicle pilot
+      li = cElem('LI', filmController.getVehiclePilot(vehicle.pilot));
+      li.setAttribute('class', 'pghi-card__body--list-item');
+      span = cElem('SPAN', "Pilot: ");
+      li.prepend(span);
+      ul.appendChild(li);
+
+      // vehicle length
+      li = cElem('LI', vehicle.length);
+      li.setAttribute('class', 'pghi-card__body--list-item');
       span = cElem('SPAN', "Length: ");
-      p.prepend(span);
-      article.appendChild(p);
+      li.prepend(span);
+      ul.appendChild(li);
 
-      p = cElem('P', vehicle.description);
-      article.appendChild(p);
-      parent.appendChild(article);
+      // vehicle description
+      li = cElem('LI', vehicle.description);
+      li.setAttribute('class', 'pghi-card__body--list-item');
+      ul.appendChild(li);
+
+      // put it all together
+      div.appendChild(ul);
+      article.append(div);
+      modalBody.appendChild(article);
     });
 
+    modalPopup.append(modalBody);
     // render the species block
-    this.render(parent);
+    this.render(modalPopup);
   },
   render: function(view) {
-    const container = document.querySelector('#pghiExtrasVehicles');
+    const container = document.querySelector('#pGhiExtrasVehiclesPopup');
     container.appendChild(view);
   }
 }
