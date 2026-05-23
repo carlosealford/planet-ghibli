ddfd6c675abd4140ac3deb5913e9095f4a1ef2be
Carlos E Alford <webshuriken@gmail.com>
2026-05-23 20:57:49 +0100

    feat: include html-loader in the webpack.config.ts

--- webpack.config.ts
+++ webpack.config.ts
@@ -154,6 +154,10 @@ export default (
           ],
         },
         {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        {
           test: /\.(png|jpe?g|gif|svg)$/i,
           type: "asset",
           parser: {
